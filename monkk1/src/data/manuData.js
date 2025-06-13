23b04600d7294a7fbc36c5a9e1971e357c5d0e8b
kuaykuay224 <suchanan343@gmail.com>
2025-06-13 11:56:58 +0700

    kao

---
+++ monkk1/src/data/manuData.js
@@ -0,0 +1,14 @@
+
+export const menuItem =[{
+    id:1,
+    name:"ข้าวราดผัดตับ",
+    price:"35",
+    image:"data:image/https://www.tomjokefoodtv.com/wp-content/uploads/2022/12/%E0%B8%9C%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B8%B1%E0%B8%9A%E0%B9%84%E0%B8%81%E0%B9%88-1.jpg",
+    desc:"ข้าวราดผัดตับ ที่น้องโน่ชอบกิน"
+},{
+     id:2,
+    name:"ข้าวกะเพรา",
+    price:"35",
+    image:"data:image/https://www.tomjokefoodtv.com/wp-content/uploads/2022/12/%E0%B8%9C%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B8%B1%E0%B8%9A%E0%B9%84%E0%B8%81%E0%B9%88-1.jpg",
+    desc:"ข้าวกะเพรา ที่น้องโน่ชอบกิน"
+}]
