ec6e87e22b320a165f49f5979ac68a7ff7350e35
Chinapat88 <chinnphathth07@gmail.com>
2025-06-20 11:20:33 +0700

    update

---
+++ monkk1/src/services/api.js
@@ -0,0 +1,28 @@
+
+const BASE_URL = 'https://dummyjson.com/';
+
+export const api ={
+    getProducts: async( limit = 30 ,skip =0)=>{
+   const response = await fetch( `${BASE_URL}/products?limit=${limit}&skip=${skip}`);
+   return response.json();
+    },
+
+    getProducts: async(id)=>{
+        const response =await fetch(`${BASE_URL}/products/${id}`)
+        return response.json();
+    },
+
+    searchProducts: async(query)=>{
+        const response =await fetch(`${BASE_URL}/products/search?q=${query}`);
+        return response.json();
+    }, 
+    getCategories : async () =>{
+        const response =await fetch(`${BASE_URL}/products/catagories`);
+        return response.json();
+    },
+
+    getProductsByCategory: async (category)=>{
+         const response =await fetch(`${BASE_URL}/products/category/${category}`);
+        return response.json();
+    }
+}
